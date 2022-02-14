0a204bbd1cb35c55c298820ab6e92b2caecbd6b0
Simon <simon2022@github.com>
2022-02-15 00:11:27 +0100

    Implementierung Netzwerk

--- LED-Controller/LED-Controller.ino
+++ LED-Controller/LED-Controller.ino
@@ -1,127 +1,75 @@
-#define STATUS_BLUE D3
-#define STATUS_RED D8
-#define STATUS_GREEN D7
+#include "VERSION.h"
+#include "PRIVATE.h"
 
+#include <ESP8266WiFi.h>
+
+WiFiClient client;
+const char* HOST = "192.168.21.25";
+const uint16_t PORT = 18533;
+
+String uuid;
 
 void setup() {
+  uuid = WiFi.macAddress();
+  
   Serial.begin(115200);
-  
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  pinMode(STATUS_BLUE, OUTPUT);
-  pinMode(STATUS_RED, OUTPUT);
-  pinMode(STATUS_GREEN, OUTPUT);
-
-  pinMode(D5, OUTPUT);
-  pinMode(D6, OUTPUT);
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-
-  digitalWrite(D5, LOW);
-  digitalWrite(D6, LOW);
-  digitalWrite(D1, LOW);
-  digitalWrite(D2, LOW);
-
-  pinMode(D0, OUTPUT);
-  digitalWrite(D0, HIGH);
-  
-  
-  
-
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-
-
-  digitalWrite(STATUS_BLUE, HIGH);
-  delay(500);
-  for(int i = 255; i >= 0; i--) {
-    Serial.println(i);
-    analogWrite(STATUS_BLUE, i);
-    delay(25);
-  }
-  digitalWrite(STATUS_BLUE, LOW);
-
   delay(1000);
-  for(int i = 0; i <= 255; i++) {
-    Serial.println(i);
-    analogWrite(STATUS_BLUE, i);
-    delay(25);
+
+  Serial.println("");
+  Serial.println("LED-Controller");
+ 
+  WiFi.hostname("LED-Controller");
+  WiFi.begin(S_WIFI_SSID, S_WIFI_PASS);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+ 
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+  if (client.connect(HOST, PORT)) {
+    client.print("REGISTER ");
+    client.println(uuid);
   }
 
-  digitalWrite(STATUS_BLUE, HIGH);
-    
 }
 
-unsigned long lastChange = millis();
-boolean lastOn = true;
+unsigned long lastPing = millis();
 
 void loop() {
-  
-  digitalWrite(STATUS_GREEN, LOW);
-  digitalWrite(STATUS_RED, LOW);
-  digitalWrite(STATUS_BLUE, HIGH);
-  
-  allOff();
-
-  digitalWrite(STATUS_BLUE, LOW);
-  digitalWrite(STATUS_RED, HIGH);
-  while(digitalRead(D0) == HIGH) {
+  while(true) {
+    if((millis() - lastPing) > 20000) {
+      Serial.println("timeout");
+      ESP.restart();
+    }
+    
+    boolean finish = false;
+    String result = "";
+    unsigned long timeout = millis();
+    while(!finish && ((millis() - timeout) < 5000)) {
+      while (client.available()) {
+        char c = static_cast<char>(client.read());
+        if(c != '\n') {
+          result.concat(c);
+        } else {
+          finish = true;
+          break;
+        }
+        yield();
+      }
+      yield();
+    }
+    if(finish) {
+      Serial.println(result);
+      if(result == "ping") {
+        client.println("pong");
+        lastPing = millis();
+      }
+    }
     yield();
   }
-  Serial.println("BTN");
-  
-  digitalWrite(STATUS_GREEN, LOW);
-  digitalWrite(STATUS_RED, LOW);
-  digitalWrite(STATUS_BLUE, HIGH);
-  allOn();
-  digitalWrite(STATUS_BLUE, LOW);
-  digitalWrite(STATUS_GREEN, HIGH);
-
-  while(digitalRead(D0) == HIGH) {
-    yield();
-  }
-  Serial.println("BTN");
-  
-}
-
-
-void allOff() {
-  for(int i = 255; i >= 0; i--) {
-    analogWrite(D2, i); 
-    delay(10);
-  }
-  for(int i = 255; i >= 0; i--) {
-    analogWrite(D1, i);
-    delay(10);
-  }
-  for(int i = 255; i >= 0; i--) {
-    analogWrite(D6, i);
-    delay(10);
-  }
-  for(int i = 255; i >= 0; i--) {
-    analogWrite(D5, i);
-    delay(10);
-  }
-}
-
-void allOn() {
-  for(int i = 0; i <= 255; i++) {
-    analogWrite(D5, i);
-    delay(10);
-  }
-  for(int i = 0; i <= 255; i++) {
-    analogWrite(D6, i);
-    delay(10);
-  }
-  for(int i = 0; i <= 255; i++) {
-    analogWrite(D1, i);
-    delay(10);
-  }
-  for(int i = 0; i <= 255; i++) {
-    analogWrite(D2, i);
-    delay(10);
-  }
-  
 }
---
+++ LED-Controller/VERSION.h
@@ -0,0 +1,1 @@
+#define S_VERSION F("0.0.1")
